41080c05720dd701c047d7945ca63f106226c61f
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-01-18 21:00:32 +0545

    updated

--- client/src/components/Homebody.jsx
+++ client/src/components/Homebody.jsx
@@ -4,7 +4,7 @@ import Img1 from '../assets/images/img1.JPG'
 function Homebody() {
   return (
     <div>
-       <div><h1>Welcome to my Blog site</h1></div>
+       <div><h1>Hello there! <br />Welcome to my Blog site</h1></div>
        <div>
         <img src="Img1" alt="" />
        </div>
