3189c7f32f421cde4138732e69f5abb75af92e85
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-01-20 21:19:15 +0545

    updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,17 +10,33 @@ function Navbar() {
   }
   return (
     
-      <nav className="bg-stone-400 shadow-md">
-        <div className="max-w-screen-lg mx-auto px-6 lg:px-16 py-4 flex justify-between items-center">
-          {/* Logo */}
-          <a href="#" className="text-xl font-bold text-gray-800 lg: ">Personal blogs</a>
+      <nav className="bg-stone-300 shadow-md relative h-24">
+        
+        
 
-          {/* links (hidden in mobile) */}
-          <div className="hidden lg:flex space-x-8">
-            <a href="home" className="text-gray-600 hover:text-black">Home</a>
-            <a href="home" className="text-gray-600 hover:text-black">About me</a>
-            <a href="home" className="text-gray-600 hover:text-black">Blogs</a>
-          </div>
+          {/* Logo at the far left corner */}
+      <div className="absolute left-4 top-9 text-xl font-bold text-gray-800 lg:text-4xl">
+        Personal Blogs
+      </div>
+          
+
+          {/* Navbar container for links */}
+      <div className="max-w-screen-lg mx-auto px-6 lg:px-16 py-4 flex items-center justify-between ">
+        {/* Spacer for the logo */}
+        <div></div>
+
+        {/* Links (hidden in mobile) */}
+        <div className="hidden lg:flex space-x-8 mt-8 text-xl">
+          <a href="/" className="text-gray-600 hover:text-black">
+            Home
+          </a>
+          <a href="/about" className="text-gray-600 hover:text-black">
+            About me
+          </a>
+          <a href="/blogs" className="text-gray-600 hover:text-black">
+            Blogs
+          </a>
+        </div>
           {/* Mobile menu button */}
           <button 
           onClick={toggleMenu} 
@@ -28,7 +44,7 @@ function Navbar() {
           >
              <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="h-16 w-11"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -49,7 +65,7 @@ function Navbar() {
           isMenuOpen ? "block" : "hidden"
         }`}
       >
-        <Link to="/home" className="block px-4 py-2 text-gray-600 hover:bg-gray-200">
+        <Link to="/" className="block px-4 py-2 text-gray-600 hover:bg-gray-200">
           Home
         </Link>
         <Link to="/about" className="block px-4 py-2 text-gray-600 hover:bg-gray-200">
