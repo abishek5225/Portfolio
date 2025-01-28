edddcad973bc83d6426966fe202ba707e069b389
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-01-28 19:14:05 +0545

    updated

--- server/index.js
+++ server/index.js
@@ -3,38 +3,17 @@ const router = express.Router();
 
 
 const cors = require('cors');
-app.use(cors());
+router.use(cors());
+router.port = process.env.PORT || 3000;
 
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));  
 
-let blogs = [
-  { id: 1, title: "Sample Blog 1", content: "Content of Blog 1", views: 120 },
-  { id: 2, title: "Sample Blog 2", content: "Content of Blog 2", views: 95 },
-  {id : 3, title: "Sample Blog 3", content: "Content of Blog 3", views: 100}
-];
-
-// Fetch all blogs
-router.get("/blogs", (req, res) => {
-  res.json(blogs);
+router.get("/", (req, res) => {
+  res.send("Welcome to the API");
 });
 
-// Add a new blog
-router.post("/blogs", (req, res) => {
-  const { title, content } = req.body;
-  if (!title || !content) {
-    return res.status(400).json({ message: "Title and Content are required!" });
-  }
-  const newBlog = {
-    id: blogs.length + 1,
-    title,
-    content,
-    views: 0,
-  };
-  blogs.push(newBlog);
-  res.status(201).json(newBlog);
-});
-
-module.exports = router;
-
-router.Listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+router.listen(port, ()=>{
+  console.log(`Server is running on port ${port}`);
+})
