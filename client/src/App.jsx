18c9e19b537df296427c5acbda8e97e81401f48b
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-02-09 19:52:22 +0545

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./Pages/Home";
+import Cv from "./Pages/CV";
 
 
 function App() {
