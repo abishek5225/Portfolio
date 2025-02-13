f6605b1c6473162de1c8051795647db91575c778
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-02-13 23:02:12 +0545

    updated

--- client/src/components/Homebody.jsx
+++ client/src/components/Homebody.jsx
@@ -8,13 +8,13 @@ function Homebody() {
   <div class="max-w-screen-lg mx-auto px-6 lg:px-16 grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
     
     <div>
-      <h1 class="text-2xl lg:text-4xl font-bold mb-4">
+      <h1 class="text-3xl lg:text-5xl font-bold mb-4">
         I'm <span class="text-black">Abishek Chaulagain</span>.
       </h1>
-      <p class="text-lg lg:text-xl text-gray-600 mb-8">
+      <p class="text-2xl lg:text-3xl text-gray-600 mb-8">
         A <span class="bg-yellow-200 text-black px-1">Tech enthusiast</span>and Full Stack Developer.
       </p>
-      <h2 class="text-xl lg:text-3xl font-light mb-4">
+      <h2 class="text-2l lg:text-3xl font-light mb-4">
         Looking <span class="italic">for</span> my next
         <span class="font-bold">opportunity</span> to make a <span class="font-bold">change</span>.
         <br />
